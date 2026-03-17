a6983d7d6375582703936babe9c847297ea8a574
torlovskaia <t.orlovskaia@student.tudelft.nl>
2026-03-17 16:40:44 +0100

    the annotated data from the clinic

--- annotation_tools/mixed_annotater.py
+++ annotation_tools/mixed_annotater.py
@@ -10,39 +10,43 @@ PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-from config.paths import QSENSE_MIXED
+from config.paths import QSENSE_MIXED, QSENSE_CLINIC
 
 # EDIT THIS BEFORE RUNNING
 # DATA_PATH = os.path.join(QSENSE_MIXED, 'Test2')
-DATA_PATH = os.path.join(QSENSE_MIXED, 'test11_Hendrik')
+DATA_PATH = os.path.join(QSENSE_CLINIC, 'sub3')
 #THRESHOLD = time(14, 57, 0)
 
 
-LABELS = np.array([1, 0])
-# np.array([0, # stand still
-#                    1, # walk natural
-#                    0, # kitchen
-#                    1, # walk, slow
-#                    0, # sit and eat smth
-#                    1, # walk slow, stroke 
-#                    0, # pretend to vacuum - right hand 
-#                    1, # walk with various speeds 
-#                    0, # stand with conve
-#                    1, # walk up and down
-#                    0]) 
-TIMESTAMPS = [time(15, 27, 56)]
-# [time(14, 54, 59), 
-#               time(14, 55, 39), 
-#               time(14, 56, 20), 
-#               time(14, 57,  6), 
-#               time(14, 57, 52), 
-#               time(14, 58, 35),
-#               time(14, 59, 18), 
-#               time(15, 00, 10), 
-#               time(15, 00, 54),
-#               time(15,  1, 57)]
+LABELS = np.array([0, # still
+                   1, # walk
+                   0, # kitchen
+                   1, # walk back 
+                   0, # dominoes 
+                   1, # walking around the building 
+                   0, # magazine 
+                   1, # walking to the execrise room
+                   0, # exercise
+                   1, # walk to the plants 
+                   0, # reaching 
+                   1, # keep walking
+                   0, # watering 
+                   1]) # walk from the plants 
+TIMESTAMPS = [time(10, 29,  8), # walk to kitchen start   
+              time(10, 29, 21), # start washing hands
+              time(10, 29, 59), # walk back   
+              time(10, 30, 12), # start dominoes 
+              time(10, 32, 34), # start walking arounf the building 
+              time(10, 34,  3), # start magazine 
+              time(10, 36,  6), # start walking to the exercise room 
+              time(10, 36, 44), # exercise 
+              time(10, 37, 48), # walking to the plants 
+              time(10, 38, 17), # reach for the jug  
+              time(10, 38, 21), # keep walking 
+              time(10, 38, 28), # start watering 
+              time(10, 39,  9)] # start walking after the plants 
               
-TIME_RANGE = (time(15, 14, 25), time(15, 39, 00))
+TIME_RANGE = (time(10, 28, 35), time(10, 39, 23))
 
 def get_label(row_time, timestamps, labels):
     """
@@ -71,71 +75,71 @@ def annotate(data_path, timestamps, labels, time_range=None):
             return True
         return time_range[0] <= row_time <= time_range[1]
 
-    for file in os.listdir(data_path):
+    # for file in os.listdir(data_path):
 
-        rw_path = os.path.join(data_path, 's1_1RW.txt')
-        lw_path = os.path.join(data_path, 's2_2LW.txt')
-        rl_path = os.path.join(data_path, 's3_3RL.txt')
+    rw_path = os.path.join(data_path, 's1_1RW.txt')
+    lw_path = os.path.join(data_path, 's2_2LW.txt')
+    rl_path = os.path.join(data_path, 's3_3RL.txt')
 
-        if os.path.exists(rw_path):
-            new_file = os.path.join(data_path, 's1_1RW_ed.txt')
-            with open(rw_path, newline='') as infile, open(new_file, 'w', newline='') as outfile:
-                reader = csv.DictReader(infile, delimiter='\t')
-                fieldnames = reader.fieldnames + ['Label']
+    if os.path.exists(rw_path):
+        new_file = os.path.join(data_path, 's1_1RW_ed.txt')
+        with open(rw_path, newline='') as infile, open(new_file, 'w', newline='') as outfile:
+            reader = csv.DictReader(infile, delimiter='\t')
+            fieldnames = reader.fieldnames + ['Label']
 
-                writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
-                writer.writeheader()
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
+            writer.writeheader()
 
-                for row in reader:
-                    row_time = parse_time(row['HH:mm:ss.fff'])
-                    if not in_range(row_time):
-                        continue
-                    row['Label'] = get_label(row_time, timestamps, labels)
-                    writer.writerow(row)
+            for row in reader:
+                row_time = parse_time(row['HH:mm:ss.fff'])
+                if not in_range(row_time):
+                    continue
+                row['Label'] = get_label(row_time, timestamps, labels)
+                writer.writerow(row)
 
-            original_backup_rw = rw_path.replace('.txt', '_old.txt')
-            os.rename(rw_path, original_backup_rw)
-            os.rename(new_file, rw_path)
+        original_backup_rw = rw_path.replace('.txt', '_old.txt')
+        os.rename(rw_path, original_backup_rw)
+        os.rename(new_file, rw_path)
 
-        if os.path.exists(lw_path):
-            new_file = os.path.join(data_path, 's2_2LW_ed.txt')
-            with open(lw_path, newline='') as infile, open(new_file, 'w', newline='') as outfile:
-                reader = csv.DictReader(infile, delimiter='\t')
-                fieldnames = reader.fieldnames + ['Label']
+    if os.path.exists(lw_path):
+        new_file = os.path.join(data_path, 's2_2LW_ed.txt')
+        with open(lw_path, newline='') as infile, open(new_file, 'w', newline='') as outfile:
+            reader = csv.DictReader(infile, delimiter='\t')
+            fieldnames = reader.fieldnames + ['Label']
 
-                writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
-                writer.writeheader()
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
+            writer.writeheader()
 
-                for row in reader:
-                    row_time = parse_time(row['HH:mm:ss.fff'])
-                    if not in_range(row_time):
-                        continue
-                    row['Label'] = get_label(row_time, timestamps, labels)
-                    writer.writerow(row)
+            for row in reader:
+                row_time = parse_time(row['HH:mm:ss.fff'])
+                if not in_range(row_time):
+                    continue
+                row['Label'] = get_label(row_time, timestamps, labels)
+                writer.writerow(row)
 
-            original_backup_lw = lw_path.replace('.txt', '_old.txt')
-            os.rename(lw_path, original_backup_lw)
-            os.rename(new_file, lw_path)
+        original_backup_lw = lw_path.replace('.txt', '_old.txt')
+        os.rename(lw_path, original_backup_lw)
+        os.rename(new_file, lw_path)
 
-        if os.path.exists(rl_path):
-            new_file = os.path.join(data_path, 's3_3RL_ed.txt')
-            with open(rl_path, newline='') as infile, open(new_file, 'w', newline='') as outfile:
-                reader = csv.DictReader(infile, delimiter='\t')
-                fieldnames = reader.fieldnames + ['Label']
+    if os.path.exists(rl_path):
+        new_file = os.path.join(data_path, 's3_3RL_ed.txt')
+        with open(rl_path, newline='') as infile, open(new_file, 'w', newline='') as outfile:
+            reader = csv.DictReader(infile, delimiter='\t')
+            fieldnames = reader.fieldnames + ['Label']
 
-                writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
-                writer.writeheader()
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames, delimiter='\t')
+            writer.writeheader()
 
-                for row in reader:
-                    row_time = parse_time(row['HH:mm:ss.fff'])
-                    if not in_range(row_time):
-                        continue
-                    row['Label'] = get_label(row_time, timestamps, labels)
-                    writer.writerow(row)
+            for row in reader:
+                row_time = parse_time(row['HH:mm:ss.fff'])
+                if not in_range(row_time):
+                    continue
+                row['Label'] = get_label(row_time, timestamps, labels)
+                writer.writerow(row)
 
-            original_backup_rl = rl_path.replace('.txt', '_old.txt')
-            os.rename(rl_path, original_backup_rl)
-            os.rename(new_file, rl_path)
+        original_backup_rl = rl_path.replace('.txt', '_old.txt')
+        os.rename(rl_path, original_backup_rl)
+        os.rename(new_file, rl_path)
 
 
 if __name__ == "__main__":
