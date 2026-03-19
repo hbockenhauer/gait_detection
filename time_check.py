17749a04b2628aca7d25bb1b2d2b0d03d6eae3e7
torlovskaia <t.orlovskaia@student.tudelft.nl>
2026-03-19 10:32:39 +0100

    adjusted data, check for faulty timestamps

--- time_check.py
+++ time_check.py
@@ -5,9 +5,9 @@ from datetime import time
 
 
 #DATA_PATH = r"C:\Users\orlov\intern\gait_detection\QSense_data_edge"
-DATA_ROOT = r"C:\Users\orlov\intern\gait_detection\QSense_tests"
+DATA_ROOT = r"C:\Users\orlov\intern\gait_detection\QSense_data_clinic"
 
-SAVE_PATH = r"C:\Users\orlov\intern\gait_detection\faulty_data_plots\tests"
+SAVE_PATH = r"C:\Users\orlov\intern\gait_detection\faulty_data_plots\clinic"
 
 FILES = [
     "s0_Hub.txt",
